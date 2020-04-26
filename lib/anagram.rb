a38d6b3252a0fed9ea1fd6b0b780d5890bd47e81
Dhrubo Talukder <dhrubo.dh@gmail.com>
2020-04-26 03:34:06 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,13 @@
 # Your code goes here!
 class Anagram
   
-  attr_accessor :word, :words_array
+  attr_accessor :word
   
   def initialize(word)
     @word = word
   end
   
   def match(words_array)
-    
+    words_array.map {|word| word.sort == @word.sort}
   end
 end
