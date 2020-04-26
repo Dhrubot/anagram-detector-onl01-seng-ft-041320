b0c0f288c942a07715185437f8c92670aed43549
Dhrubo Talukder <dhrubo.dh@gmail.com>
2020-04-26 03:37:17 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ class Anagram
   
   def match(words_array)
     anagram_word = []
-    anagram_word << words_array.map {|word| word.to_a.sort == @word.to_a.sort}
+    anagram_word << words_array.map {|word| word.split("").sort == @word.split("").sort}
     anagram_word
   end
 end
